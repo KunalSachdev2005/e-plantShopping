cbe05cb578c60fd01b2eedaffbd12b9de7e956bf
KunalSachdev2005 <kunalmgsachdev@gmail.com>
2025-01-26 02:32:37 -0500

    Finished CartItems Component

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,11 +2,14 @@ import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
+import { useSelector, useDispatch } from 'react-redux';
 
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addToCart, setAddToCart] = useState({});
+    const cartItems = useSelector((state) => state.cart.items);
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
@@ -215,6 +218,8 @@ function ProductList() {
             ]
         }
     ];
+
+    const totalQuantity = cartItems.reduce(total, item)
 
    const styleObj={
     backgroundColor: '#4CAF50',
